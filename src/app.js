fe9a2bc85ce45b097f06c98ee40699f12fc8bbea
amaralc <amaral.calil@gmail.com>
2020-01-18 01:29:25 -0300

    style: faz linting do codigo

--- src/app.js
+++ src/app.js
@@ -5,23 +5,23 @@ import routes from './routes';
 // CONTENT ---------------------------------------------------------------------
 class App {
   // chamado automaticamente quando classe for instanciada
-  constructor(){
+  constructor() {
     this.server = express();
     this.middlewares();
     this.routes();
   }
 
   // cadastro dos middlewares da aplicacao
-  middlewares(){
+  middlewares() {
     // prepara app para receber requisicoes em formato json
     this.server.use(express.json());
   }
 
-  routes(){
+  routes() {
     // importa rotas de outro arquivo e usa no servidor
     this.server.use(routes);
   }
 }
 
 // EXPORTS ---------------------------------------------------------------------
-export default new App().server; 
+export default new App().server;
