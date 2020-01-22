1764ff9de1706fb923428b5d8d411f0b11e60620
amaralc <amaral.calil@gmail.com>
2020-01-22 00:05:44 -0300

    API-CLIENT / FEATURE / Cria rota de teste para criar e retornar usuario

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
 // IMPORTS  --------------------------------------------------------------------
 import { Router } from 'express';
+import User from './app/models/User';
 
 // CONTENT ---------------------------------------------------------------------
 const routes = new Router();
 
 // define rota raiz
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+routes.get('/', async (req, res) => {
+  /*
+   ** Cria usuario teste na rota raiz para avaliar se loader de models esta funcionando
+   */
+  const user = await User.create({
+    name: 'usuario numero um',
+    email: 'user1@email.com',
+    password_hash: '123456',
+  });
+
+  /* Altera res.json() para retornar objeto com usuario criado */
+  return res.json(user);
 });
 
 // EXPORTS  ---------------------------------------------------------------------
