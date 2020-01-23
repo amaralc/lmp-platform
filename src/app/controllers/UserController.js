21bca14fa7b8d7b24fa013b3fde8f297d35c4332
amaralc <amaral.calil@gmail.com>
2020-01-23 16:50:05 -0300

    API-CLIENT / FEATURE / Cria UserController e rota '/users' tipo post

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,36 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
+import User from '../models/User';
+
+/* --------------------------------- CONTENT ---------------------------------*/
+class UserController {
+  /**
+   * Metodo store com mesma face de um middleware no node.
+   * Recebe dados do usuario e cria novo registro dentro da base de dados.
+   */
+  async store(req, res) {
+    /** Verifica se usuario do corpo da requisicao ja existe */
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    /** Se usuario ja existir, retorna erro */
+    if (userExists) {
+      return res.json({ error: 'User already exists!' });
+    }
+
+    /**
+     * Cria usuario na base de dados usando resposta asincrona e retorna apenas
+     * dados uteis.
+     */
+    const { id, name, email, provider } = await User.create(req.body);
+
+    /** Retorna json apenas com dados uteis ao frontend */
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+/* --------------------------------- EXPORTS ---------------------------------*/
+export default new UserController();
