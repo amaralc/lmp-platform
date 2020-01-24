9a45f92a71291ccbff499a9aad02fdcc22470ead
amaralc <amaral.calil@gmail.com>
2020-01-23 23:34:46 -0300

    API-CLIENT / FEATURE / Cria arquivo com configuracao do token

---
+++ src/config/auth.js
@@ -0,0 +1,7 @@
+/* --------------------------------- EXPORTS ---------------------------------*/
+export default {
+  /** String secreta aleatoria (ex.: gerada no md5online.org) */
+  secret: '5ed32cb43d6810f8b9271a6858613e94',
+  /** Envia data de expiracao obrigatoria do token (padrao: 7 dias) */
+  expiresIn: '7d',
+};
