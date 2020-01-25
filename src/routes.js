1051bf39f80c334b31ae2b1e1dcfe2a67f57bceb
amaralc <amaral.calil@gmail.com>
2020-01-25 17:42:39 -0300

    API-CLIENT / FEATURE / Adiciona middleware de autenticacao a rotas da aplicacao

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,20 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 /* --------------------------------- CONTENT ---------------------------------*/
 const routes = new Router();
 
-/** Define rota post para criar novo usuario */
+/** Define rota PUT para criar novo usuario */
 routes.post('/users', UserController.store);
-/** Define rota post para criar nova session */
+/** Define rota POST para criar nova session */
 routes.post('/sessions', SessionController.store);
+
+/** Define MIDDLEWARE GLOBAL que vale para rotas que vem apos sua declaracao */
+routes.use(authMiddleware);
+/** Define rota PUT para editar dados do usuario */
+routes.put('/users', UserController.update);
 
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default routes;
