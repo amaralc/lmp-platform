b0524da6b19d6bf7ec276c5d69fd5bbf69951da1
amaralc <amaral.calil@gmail.com>
2020-01-26 01:35:33 -0300

    API-CLIENT / FEATURE / Add schema yup schema validation to SessionController

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,11 +1,28 @@
 /* --------------------------------- IMPORTS ---------------------------------*/
 import jwt from 'jsonwebtoken';
+/** Importa tudo de yup como Yup (dependencia nao tem export default) */
+import * as Yup from 'yup';
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 /* --------------------------------- CONTENT ---------------------------------*/
 class SessionController {
   async store(req, res) {
+    /** Define schema to validate req.body prior to 'store()' data */
+    const schema = Yup.object().shape({
+      /** Attribute 'email' is a required string with email format */
+      email: Yup.string()
+        .email()
+        .required(),
+      /** Attribute 'password' is a required string */
+      password: Yup.string().required(),
+    });
+
+    /** If 'req.body' do not attend to the schema requirements (is not valid) */
+    if (!(await schema.isValid(req.body))) {
+      /** Return error status 400 with message 'Validation has failed' */
+      return res.status(400).json({ error: 'Validation has failed' });
+    }
     /** Salva email e senha recebidos no corpo da requisicao */
     const { email, password } = req.body;
 
