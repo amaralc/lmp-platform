c339ab7a733862913aecb59ecc64c69bbfb317f9
amaralc <amaral.calil@gmail.com>
2020-01-27 22:52:32 -0300

    API-CLIENT / CONFIGURATION / Atualiza base de dados, autenticacao e documentacao

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,7 @@
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default {
   /** String secreta aleatoria (ex.: gerada no md5online.org) */
-  secret: '5ed32cb43d6810f8b9271a6858613e94',
+  secret: 'e73f038641d3a448df9a9c80aaf7265a',
   /** Envia data de expiracao obrigatoria do token (padrao: 7 dias) */
   expiresIn: '7d',
 };
