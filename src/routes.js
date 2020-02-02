978fa24ef82d89117a316e8d513380638e0c6c5c
amaralc <amaral.calil@gmail.com>
2020-02-02 00:30:22 -0300

    API-CLIENT / FEATURE / Create 'multer' configuration file and route for uploads

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 /* --------------------------------- IMPORTS ---------------------------------*/
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 /* --------------------------------- CONTENT ---------------------------------*/
+/** Instancia novo roteador Router do express */
 const routes = new Router();
+/** Variavel de configuracao do upload usando multer */
+const upload = multer(multerConfig);
 
 /** Define rota PUT para criar novo usuario */
 routes.post('/users', UserController.store);
@@ -16,6 +21,13 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 /** Define rota PUT para editar dados do usuario */
 routes.put('/users', UserController.update);
+/**
+ * Define rota POST para upload de arquivos (com middleware e controller local, sem arquivo separado)
+ * Middleware chama variavel upload, metodo 'single' para fazer upload de um arquivo por vez
+ */
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default routes;
