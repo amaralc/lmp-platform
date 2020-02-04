7b27a40f69b439c0dd3325d9bdaae999e247df58
amaralc <amaral.calil@gmail.com>
2020-02-04 15:38:10 -0300

    API-CLIENT / FEATURE / Cria file controller e move metodo store para routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 /* --------------------------------- CONTENT ---------------------------------*/
 /** Instancia novo roteador Router do express */
@@ -22,12 +23,11 @@ routes.use(authMiddleware);
 /** Define rota PUT para editar dados do usuario */
 routes.put('/users', UserController.update);
 /**
- * Define rota POST para upload de arquivos (com middleware e controller local, sem arquivo separado)
- * Middleware chama variavel upload, metodo 'single' para fazer upload de um arquivo por vez
+ * Define rota POST para upload de arquivos (com middleware local)
+ * Middleware chama variavel upload, metodo 'single' para fazer upload de
+ * um arquivo por vez
  */
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default routes;
