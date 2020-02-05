b661ad94d6d53c53e0d8d101d2b6359c96236da2
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-02-05 13:26:53 -0300

    API-CLIENT / FEATURE / Modifica o arquivo routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import RoomController from './app/controllers/RoomController';
 
 /* --------------------------------- CONTENT ---------------------------------*/
 const routes = new Router();
@@ -11,11 +12,15 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 /** Define rota POST para criar nova session */
 routes.post('/sessions', SessionController.store);
+/** Define rota POST para criar nova sala */
+routes.post('/rooms', RoomController.store);
 
 /** Define MIDDLEWARE GLOBAL que vale para rotas que vem apos sua declaracao */
 routes.use(authMiddleware);
 /** Define rota PUT para editar dados do usuario */
 routes.put('/users', UserController.update);
+/** Define rota PUT para editar dados da sala */
+routes.put('/rooms', RoomController.update);
 
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default routes;
