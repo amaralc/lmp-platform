ecbb1943f06956dfb21215a8b8215b909ff36512
Luam Maul <luamgmaul@gmail.com>
2020-02-06 09:16:15 -0400

    API-CLIENT / FEATURE / Criar model Equipments

--- src/database/migrations/20200205002642-create-equipments.js
+++ src/database/migrations/20200205002642-create-equipments.js
@@ -14,7 +14,7 @@ module.exports = {
         allowNull: false,
       },
       // Nome do equipamento (Torno, Torno CNC, Fresadora)
-      equipament: {
+      equipment: {
         type: Sequelize.STRING,
         allowNull: false,
       },
