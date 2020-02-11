2233d76badb815c72a55bb005a50c91218265ed5
Artur Della Favera <arturdellafavera@gmail.com>
2020-02-11 11:08:58 -0300

    API-CLIENT / FEATURE / Cria método update em RoomController.js

--- src/app/controllers/RoomController.js
+++ src/app/controllers/RoomController.js
@@ -49,6 +49,47 @@ class RoomController {
       container,
     });
   }
+
+  /* ----- ALTERAÇÃO DE DADOS DA SALA (UPDATE) ----- */
+
+  /** Metodo de alteracao dos dados da sala */
+  async update(req, res) {
+    /** Define schema to validate req.body prior to 'update()' method */
+    const schema = Yup.object().shape({
+      /** Attribute 'id' */
+      id: Yup.number().required(),
+      /** Attribute 'number' is a required number */
+      number: Yup.number(),
+      /** Attribute 'lab' is a required string */
+      lab: Yup.number(),
+      /** Attribute 'description' is a required string */
+      description: Yup.string(),
+      /** Attribute 'container' is a required number */
+      containers: Yup.number(),
+    });
+    /** If 'req.body' do not attend to the schema requirements (is not valid) */
+    if (!(await schema.isValid(req.body))) {
+      /** Return error status 400 with message 'Validation has failed' */
+      return res.status(400).json({ error: 'Validation has failed' });
+    }
+
+    /** Get current room information */
+    const room = await Room.findByPk(req.body.id);
+
+    /** If all requirements were met then updates room information */
+    const { id, number, lab, description, containers } = await room.update(
+      req.body
+    );
+
+    /** Retorna json apenas com dados uteis ao frontend */
+    return res.json({
+      id,
+      number,
+      lab,
+      description,
+      containers,
+    });
+  }
 }
 
 /* --------------------------------- EXPORTS ---------------------------------*/
