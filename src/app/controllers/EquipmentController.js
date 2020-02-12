c3fff3f62e7a22901866d59180d4436fa0126178
Artur Della Favera <arturdellafavera@gmail.com>
2020-02-12 16:41:21 -0300

    API-CLIENT / FEATURE / Cria validação por yup e update para equipments

--- src/app/controllers/EquipmentController.js
+++ src/app/controllers/EquipmentController.js
@@ -1,101 +1,150 @@
 /* --------------------------------- IMPORTS ---------------------------------*/
+/** Importa tudo de yup como Yup (dependencia nao tem export default) */
+import * as Yup from 'yup';
 import Equipment from '../models/Equipment';
+
 /* --------------------------------- CONTENT ---------------------------------*/
 class EquipmentController {
-  // Método Store: receberá dados pelo body e registrará novo equipamento no database...
+  /**
+   * Metodo store com mesma face de um middleware no node.
+   * Recebe dados do usuario e cria novo registro dentro da base de dados.
+   */
   async store(req, res) {
-    // ... Verifica se já existe dentro da database algum cadastro que possua o mesmo valor em ufsc_patrimony...
+    /** Define schema to validate req.body prior to 'store()' data */
+    const schema = Yup.object().shape({
+      /** Attribute 'category' is a required string */
+      category: Yup.string().required(),
+      /** Attribute 'equipment_name' is a required string */
+      equipment_name: Yup.string().required(),
+      /** Attribute 'company' is a required string */
+      company: Yup.string().required(),
+      /** Attribute 'model' is a required string */
+      model: Yup.string().required(),
+      /** Attribute 'ufsc_patrimony' is a required number */
+      ufsc_patrimony: Yup.number().required(),
+      /** Attribute 'feesc_patrimony' is a required number */
+      feesc_patrimony: Yup.number().required(),
+      /** Attribute 'color' is a required string */
+      color: Yup.string().required(),
+      /** Attribute 'serial_number' is a required string */
+      serial_number: Yup.string().required(),
+      /** Attribute 'comments' is a required string */
+      comments: Yup.string().required(),
+      /** Attribute 'state' is a required string */
+      state: Yup.string().required(),
+      /** Attribute 'image' is a required number */
+      image: Yup.string().required(),
+    });
+
+    /** If 'req.body' do not attend to the schema requirements (is not valid) */
+    if (!(await schema.isValid(req.body))) {
+      /** Return error status 400 with message 'Validation has failed' */
+      return res.status(400).json({ error: 'Validation has failed' });
+    }
+
+    /** Verifica se o valor de ufsc_patrimony do corpo da requisicao ja existe */
     const ufsc_patrimonyExists = await Equipment.findOne({
       where: { ufsc_patrimony: req.body.ufsc_patrimony },
     });
-
-    // ... Caso exista, recusa o novo registro e retorna uma mensagem de erro informando que tal equipamento já está cadastrado...
+    /** Se valor de ufsc_patrimony ja existir, retorna erro */
     if (ufsc_patrimonyExists) {
       return res
         .status(400)
         .json({ error: 'UFSC Patrimony number already exists.' });
     }
 
-    // Faz a mesma consulta para a variável feesc_patrimony...
+    /** Verifica se o valor de feesc_patrimony do corpo da requisicao ja existe */
     const feesc_patrimonyExists = await Equipment.findOne({
       where: { feesc_patrimony: req.body.feesc_patrimony },
     });
-
-    // ... E retorna o erro caso exista.
+    /** Se valor de feesc_patrimony ja existir, retorna erro */
     if (feesc_patrimonyExists) {
       return res
         .status(400)
         .json({ error: 'FEESC Patrimony number already exists.' });
     }
-
-    // Caso o equipamento não exista na database, ele é registrado...
-    const equipment = await Equipment.create(req.body);
-
-    // ... E retorna-se os valores registrados.
-    return res.json(equipment);
-  }
-
-  // Método Update: receberá os dados pelo body, verificará se tal equipamento existe e atualizará suas informações
-  async update(req, res) {
-    const {
-      ufsc_patrimony,
-      feesc_patrimony,
-      old_ufsc_patrimony,
-      old_feesc_patrimony,
-    } = req.body;
-
-    // Primeiro consulta para verificar existência do equipamento pelo ID...
-    const equipment = await Equipment.findByPk(req.body.id);
-
-    // ... Verifica se já existe dentro da database algum cadastro que possua o mesmo valor em ufsc_patrimony...
-    if (ufsc_patrimony !== equipment.ufsc_patrimony) {
-      const ufsc_patrimonyExists = await Equipment.findOne({
-        where: { ufsc_patrimony },
-      });
-      // ... Caso exista, recusa o novo registro e retorna uma mensagem de erro informando que tal equipamento já está cadastrado...
-      if (ufsc_patrimonyExists) {
-        return res
-          .status(400)
-          .json({ error: 'UFSC Patrimony number already exists.' });
-      }
-    }
-
-    // ... Verifica se já existe dentro da database algum cadastro que possua o mesmo valor em feesc_patrimony...
-    if (feesc_patrimony !== equipment.feesc_patrimony) {
-      const feesc_patrimonyExists = await Equipment.findOne({
-        where: { feesc_patrimony },
-      });
-
-      // ... Caso exista, recusa o novo registro e retorna uma mensagem de erro informando que tal equipamento já está cadastrado...
-      if (feesc_patrimonyExists) {
-        return res
-          .status(400)
-          .json({ error: 'FEESC Patrimony number already exists.' });
-      }
-    }
-    /*
-    if (
-      old_ufsc_patrimony &&
-      (await equipment.ufsc_patrimony) !== (await res.body.old_ufsc_patrimony)
-    ) {
-      return res.status(401).json({ error: 'UFSC Patrimony does not match' });
-    }
-
-    if (
-      old_feesc_patrimony &&
-      !(await equipment.checkPassword(old_feesc_patrimony))
-    ) {
-      return res.status(401).json({ error: 'FEESC Patrimony does not match' });
-    }
-    */
-
-    // Caso o equipamento exista e tenha passado pelas validações, ele é atualizado no database...
+    /**
+     * Cria usuario na base de dados usando resposta asincrona e retorna apenas
+     * dados uteis.
+     */
     const {
       id,
       category,
       equipment_name,
       company,
       model,
+      ufsc_patrimony,
+      feesc_patrimony,
+      color,
+      serial_number,
+      comments,
+      state,
+      image,
+    } = await Equipment.create(req.body);
+
+    /** Retorna json apenas com dados uteis ao frontend */
+    return res.json({
+      id,
+      category,
+      equipment_name,
+      company,
+      ufsc_patrimony,
+      feesc_patrimony,
+      model,
+      color,
+      serial_number,
+      comments,
+      state,
+      image,
+    });
+  }
+
+  /** Metodo de alteracao dos dados do usuario */
+  async update(req, res) {
+    /** Define schema to validate req.body prior to 'store()' data */
+    const schema = Yup.object().shape({
+      /** Attribute 'id' */
+      id: Yup.number().required(),
+      /** Attribute 'category' is a required string */
+      category: Yup.string(),
+      /** Attribute 'equipment_name' is a required string */
+      equipment_name: Yup.string(),
+      /** Attribute 'company' is a required string */
+      company: Yup.string(),
+      /** Attribute 'model' is a required string */
+      model: Yup.string(),
+      /** Attribute 'ufsc_patrimony' is a required number */
+      ufsc_patrimony: Yup.number(),
+      /** Attribute 'feesc_patrimony' is a required number */
+      feesc_patrimony: Yup.number(),
+      /** Attribute 'color' is a required string */
+      color: Yup.string(),
+      /** Attribute 'serial_number' is a required string */
+      serial_number: Yup.string(),
+      /** Attribute 'comments' is a required string */
+      comments: Yup.string(),
+      /** Attribute 'state' is a required string */
+      state: Yup.string(),
+      /** Attribute 'image' is a required number */
+      image: Yup.string(),
+    });
+    /** If 'req.body' do not attend to the schema requirements (is not valid) */
+    if (!(await schema.isValid(req.body))) {
+      /** Return error status 400 with message 'Validation has failed' */
+      return res.status(400).json({ error: 'Validation has failed' });
+    }
+
+    const equipment = await Equipment.findByPk(req.body.id);
+
+    /** If all requirements were met then updates user informaiton */
+    const {
+      id,
+      category,
+      equipment_name,
+      company,
+      model,
+      ufsc_patrimony,
+      feesc_patrimony,
       color,
       serial_number,
       comments,
@@ -104,7 +153,7 @@ class EquipmentController {
       image,
     } = await equipment.update(req.body);
 
-    // E retorna os valores registrados.
+    /** Retorna json apenas com dados uteis ao frontend */
     return res.json({
       id,
       category,
@@ -119,10 +168,9 @@ class EquipmentController {
       state,
       room_id,
       image,
-      old_ufsc_patrimony,
-      old_feesc_patrimony,
     });
   }
 }
+
 /* --------------------------------- EXPORTS ---------------------------------*/
 export default new EquipmentController();
