20eb9a2df52abde04db9d95b570ef304ef9c3c15
amaralc <32170099+amaralc@users.noreply.github.com>
2020-02-12 23:18:22 -0300

    API-CLIENT / FIX / Passa relacionamento para migration create rooms
    Assim reduzimos a quantidade de arquivos no código visto que ainda é um código simples e sem bancos de dados rodando em produção.

--- src/database/migrations/20200130211038-create-rooms.js
+++ src/database/migrations/20200130211038-create-rooms.js
@@ -19,6 +19,22 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      /** ID (primary key) do lab responsavel pelo room */
+      lab_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        /** ... que referencie... */
+        references: {
+          /** ... a tabela 'labs' */
+          model: 'labs',
+          /** ... usando a chave 'id' */
+          key: 'id',
+        },
+        /** Se 'room_id' for alterado, repasse a alteracao para tabela de container */
+        onUpdate: 'CASCADE',
+        /** Se 'room_id' for deletado, defina como nulo */
+        onDelete: 'SET NULL',
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
