6dd6e099e56fdab170887e11b8e6d0229c5752c5
amaralc <amaral.calil@gmail.com>
2020-02-18 21:44:56 -0300

    API-CLIENT / FEATURE / Add 'responsible_id' and 'hourly_rate' to migration

--- src/database/migrations/20200205002642-create-equipment.js
+++ src/database/migrations/20200205002642-create-equipment.js
@@ -13,54 +13,80 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      // Nome do equipamento (Torno, Torno CNC, Fresadora)
+
+      /** Nome do equipamento (Torno, Torno CNC, Fresadora) */
       equipment_name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      // Empresa que fabricou o equipamento
+      /** Empresa que fabricou o equipamento */
       company: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      // Modelo do equipamento
+      /** Modelo do equipamento */
       model: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      // Número de patrimônio da UFSC
+      /** Número de patrimônio da UFSC */
       ufsc_patrimony: {
         type: Sequelize.INTEGER,
         allowNull: true,
         unique: true,
       },
-      // Número de patrimônio da FEESC
+      /** Número de patrimônio da FEESC */
       feesc_patrimony: {
         type: Sequelize.INTEGER,
         allowNull: true,
         unique: true,
       },
-      // Cor do equipamento
+      /** Cor do equipamento */
       color: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      // Número de série do equipamento
+      /** Número de série do equipamento */
       serial_number: {
         type: Sequelize.STRING,
         allowNull: true,
       },
-      // Campo para descrição e comentários
+      /** Campo para descrição e comentários */
       comments: {
         type: Sequelize.STRING,
         allowNull: true,
       },
-      // Estado de funcionamento do equipamento (Funcionando, Parado ou Não encontrado)
+      /** Estado de funcionamento do equipamento (Funcionando, Parado ou Não encontrado) */
       state: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      // ID da sala da tabela Rooms
+      /** Foto do equipamento */
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      /** Valor da hora do equipamento em reais (BRL) */
+      hourly_rate_brl: {
+        type: Sequelize.DOUBLE,
+        allowNull: true,
+      },
+      /** ID (primary key) da sala do container */
+      responsible_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        /** ... que referencie... */
+        references: {
+          /** ... a tabela 'files' */
+          model: 'users',
+          /** ... usando a chave 'id' */
+          key: 'id',
+        },
+        /** Se 'room_id' for alterado, repasse a alteracao para tabela de container */
+        onUpdate: 'CASCADE',
+        /** Se 'room_id' for deletado, defina como nulo */
+        onDelete: 'SET NULL',
+      },
       /** ID (primary key) da sala onde equipamento se encontra */
       room_id: {
         type: Sequelize.INTEGER,
@@ -77,11 +103,7 @@ module.exports = {
         /** Se 'lab_id' for deletado, defina como nulo */
         onDelete: 'SET NULL',
       },
-      // Foto do equipamento
-      image: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
+      /** Timestamp de registro do equipamento */
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
@@ -90,6 +112,17 @@ module.exports = {
       created_by: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        /** ... que referencie... */
+        references: {
+          /** ... a tabela 'files' */
+          model: 'users',
+          /** ... usando a chave 'id' */
+          key: 'id',
+        },
+        /** Se 'room_id' for alterado, repasse a alteracao para tabela de container */
+        onUpdate: 'CASCADE',
+        /** Se 'room_id' for deletado, defina como nulo */
+        onDelete: 'SET NULL',
       },
       updated_at: {
         type: Sequelize.DATE,
@@ -99,6 +132,17 @@ module.exports = {
       updated_by: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        /** ... que referencie... */
+        references: {
+          /** ... a tabela 'files' */
+          model: 'users',
+          /** ... usando a chave 'id' */
+          key: 'id',
+        },
+        /** Se 'room_id' for alterado, repasse a alteracao para tabela de container */
+        onUpdate: 'CASCADE',
+        /** Se 'room_id' for deletado, defina como nulo */
+        onDelete: 'SET NULL',
       },
     });
   },
