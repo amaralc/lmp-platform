6c661a86b67f09a172538ea8db15f1fbd1aa5449
Artur Della Favera <arturdellafavera@gmail.com>
2020-02-20 08:20:26 -0300

    API-CLEINT / FEATURE / Cria arquivo mail.js com informações do remetente

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+/* --------------------------------- EXPORTS ---------------------------------*/
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'a86bd473d25895',
+    pass: '8c35644e2ddd0a',
+  },
+  default: {
+    from: 'user12 <noreply@platform.com>',
+  },
+};
