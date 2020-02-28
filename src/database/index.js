6fa25ce89690dd3459f8ed29025a9ceab51706a1
amaralc <amaral.calil@gmail.com>
2020-02-27 23:25:06 -0300

    API-CLIENT / FIX / Altera useFindAndModify para 'false'
    * https://mongoosejs.com/docs/deprecations.html#-findandmodify-
    
    * https://woliveiras.com.br/posts/construindo-uma-api-com-node-js-parte-3-atualizando-e-deletando-dados/

--- src/database/index.js
+++ src/database/index.js
@@ -56,7 +56,7 @@ class Database {
        */
       {
         useNewUrlParser: true,
-        useFindAndModify: true,
+        useFindAndModify: false,
         useUnifiedTopology: true,
       }
     );
