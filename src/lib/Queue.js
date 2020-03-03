34e9c6f3b211ffb501da67c222d42f46e531de0e
amaralc <amaral.calil@gmail.com>
2020-03-03 00:47:44 -0300

    API-CLIENT / FEATURE / Cria arquivo de conexao com o redis

---
+++ src/lib/Queue.js
@@ -0,0 +1,39 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+/* --------------------------------- CONTENT ---------------------------------*/
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+/* --------------------------------- EXPORTS ---------------------------------*/
+export default new Queue();
