c4c18fb113f2ddd2ad4fdd3236e949c8745e2c53
Artur Della Favera <arturdellafavera@gmail.com>
2020-03-03 09:52:59 -0300

    API-CLIENT / DOCUMENTATION / Adiciona comentários a queue.js

--- src/queue.js
+++ src/queue.js
@@ -2,4 +2,9 @@
 import Queue from './lib/Queue';
 
 /* --------------------------------- CONTENT ---------------------------------*/
+/**
+ * Permite com que as filas sejam processadas separadas da aplicação
+ * É possível que um terminal rode a aplicação e um segundo terminal
+ * rode as filas (utilizando o comando: yarn queue)
+ */
 Queue.processQueue();
