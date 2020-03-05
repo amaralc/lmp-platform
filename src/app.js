6a04af0641db0a543c634e2a892648ba5f8b3ade
amaralc <amaral.calil@gmail.com>
2020-03-05 17:56:24 -0300

    API-CLIENT / FEATURE / Adiciona exceptionHandler e utiliza sentry no constructor e middlewares

--- src/app.js
+++ src/app.js
@@ -24,12 +24,24 @@ class App {
   /* Chamado automaticamente quando classe for instanciada */
   constructor() {
     this.server = express();
+
+    /**
+     * Inicializa Sentry conforme disponivel em conta
+     * https://sentry.io/onboarding/amaralc/get-started/
+     */
+    Sentry.init(sentryConfig);
+
+    /** Call middlewares and routes methods */
     this.middlewares();
     this.routes();
+
+    this.exceptionHandler();
   }
 
   /* Cadastro dos middlewares da aplicacao */
   middlewares() {
+    /** The request handler must be the first middleware on the app */
+    this.server.use(Sentry.Handlers.requestHandler());
     /* Prepara app para receber requisicoes em formato json */
     this.server.use(express.json());
     /** Usaremos o recurso express.static que serve para servir arquivos estáticos (PNG, CSS, HTML), arquivos acessados diretamente pelo navegador. */
@@ -42,6 +54,21 @@ class App {
   routes() {
     /* Importa rotas de outro arquivo e usa no servidor */
     this.server.use(routes);
+    /**
+     * The error handler must be before any other error middleware and after
+     * all controllers
+     */
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      /**
+       * Retorna erro de servidor em formato json
+       */
+      return res.status(500).json(errors);
+    });
   }
 }
 
